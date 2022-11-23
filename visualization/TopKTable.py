45767abb1847c076022fa4a1b93f1e146aadac44
Beren <bmillidgework@gmail.com>
2022-11-23 11:54:03 +0000

    Add files via upload

---
+++ visualization/TopKTable.py
@@ -0,0 +1,78 @@
+from typing import List, Union, Literal
+
+import numpy as np
+import torch
+
+Tensor = Union[np.ndarray, torch.Tensor]
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def init(
+    tokens: List[str],
+    activations: Tensor,
+    obj_type: str,
+    layer_labels=None,
+    obj_labels=None,
+    k=10,
+    filter: Literal["topk", "bottomk", "topk+bottomk"] = "topk",
+):
+    """Visualize the top-k tokens and their corresponding activation strength
+    in table format for a set of objects such as SVD directions or neurons.
+
+    Args:
+      tokens: a list of strings representing tokens.
+      activations: A [n_tokens, n_layers, n_objects_per_layer] array representing activation values.
+      obj_type: A string representing the type of object. For example, "Neuron" or "SVD direction".
+      layer_labels: (optional) human readable labels for the given layers.
+      obj_labels: (optional) human readable labels for the given objects.
+      k = 10: The number of tokens to show for each object.
+      filter = "topk": The filter to use for selecting the top-k tokens. Can be "topk" or "threshold".
+    """
+    assert filter in ["topk", "bottomk", "topk+bottomk"]
+    assert isinstance(obj_type, str), "obj_type must be a string"
+    assert (
+        len(tokens) == activations.shape[0]
+    ), "tokens and activations must be same length"
+    assert (
+        len(activations.shape) == 3
+    ), "activations must be of the form [n_tokens, n_layers, n_objects_per_layer]"
+    if layer_labels is None:
+        layer_labels = [str(layer) for layer in range(activations.shape[1])]
+    else:
+        assert (
+            len(layer_labels) == activations.shape[1]
+        ), "layer_labels must correspond to number of layers in the activations"
+
+    if obj_labels is None:
+        obj_labels = [str(obj) for obj in range(activations.shape[2])]
+    else:
+        assert (
+            len(obj_labels) == activations.shape[2]
+        ), "obj_labels must correspond to number of objects in the activations"
+
+    # get topk tokens for each object
+    activations_tensor = torch.tensor(activations).to(device)
+    topk_vals, topk_inds = activations_tensor.topk(k=k, dim=0)
+
+    # also get bottom k vals
+    bottomk_vals, bottomk_inds = activations_tensor.topk(k=k, dim=0, largest=False)
+
+    # reverse sort order of bottomk vals and inds
+    bottomk_vals = bottomk_vals.flip(0)
+    bottomk_inds = bottomk_inds.flip(0)
+
+    filter = (
+        None if filter == "topk+bottomk" else filter
+    )  # defaults to topk+bottomk if filter is None
+    return {
+        "tokens": tokens,
+        "obj_type": obj_type,
+        "layer_labels": layer_labels,
+        "obj_labels": obj_labels,
+        "topk_vals": topk_vals,
+        "topk_inds": topk_inds,
+        "bottomk_vals": bottomk_vals,
+        "bottomk_inds": bottomk_inds,
+        "filter": filter,
+    }
---
+++ visualization/TopKTable.svelte
@@ -0,0 +1,271 @@
+<script>
+    //////// Pass in values for these
+    export let tokens;
+    export let obj_type; // e.g. "SVD Direction" or "Neuron"
+    export let layer_labels;
+    export let obj_labels;
+    export let topk_vals; // [k, n_layers, n_objects]
+    export let topk_inds; // [k, n_layers, n_objects]
+    export let bottomk_vals; // [k, n_layers, n_objects]
+    export let bottomk_inds; // [k, n_layers, n_objects]
+    export let filter; // default to showing top+bottom
+    ////////
+
+    export let act_val = "0.0";
+    export let focus_layer = 0; // default to first layer
+    export let focus_obj;
+
+    function idx_into_arr_from_ndarray_slice(arr, ndarray_1d_slice) {
+        if (ndarray_1d_slice.shape.length != 1) {
+            return null;
+        }
+        // ndarrays don't have a map function, so we have to do this the hard way
+        let sub_arr = [];
+        for (let i = 0; i < ndarray_1d_slice.shape[0]; i++) {
+            sub_arr.push(arr[Math.round(ndarray_1d_slice.get(i))]);
+        }
+        return sub_arr;
+    }
+
+    $: focus_topk_tokens = idx_into_arr_from_ndarray_slice(
+        tokens,
+        topk_inds.pick(null, focus_layer, focus_obj)
+    );
+
+    $: focus_bottomk_tokens = idx_into_arr_from_ndarray_slice(
+        tokens,
+        bottomk_inds.pick(null, focus_layer, focus_obj)
+    );
+
+    $: activation_text = "Value: " + act_val;
+
+    function range(n) {
+        return [...Array(n).keys()];
+    }
+
+    function onOver(item) {
+        act_val = item;
+    }
+
+    function onLeave(item) {
+        act_val = "0.0";
+    }
+
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+    const clamp = (num) => Math.min(Math.max(num, 0), 1);
+
+    function toColor(val) {
+        parseInt(clamp(val) * 255);
+        return rgbToHex(
+            255,
+            255 - parseInt(clamp(val) * 255),
+            255 - parseInt(clamp(val) * 255)
+        );
+    }
+</script>
+
+<div class="container">
+    <div class="objs-container">
+        <label class="title" for="layer-select">Layer:</label>
+        <select id="layer-select" bind:value={focus_layer} class="layer-select">
+            {#each layer_labels as label, label_idx}
+                <option value={label_idx}>{label}</option>
+            {/each}
+        </select>
+        <label class="title" for="obj-select">{obj_type}:</label>
+        <select id="obj-select" bind:value={focus_obj} class="obj-select">
+            <option value={undefined} selected>all</option>
+            {#each obj_labels as label, label_idx}
+                <option value={label_idx}>{label}</option>
+            {/each}
+        </select>
+        <label class="title" for="filter-select">Filter:</label>
+        <select id="filter-select" bind:value={filter} class="filter-select">
+            <option value={undefined} selected>topk+bottomk</option>
+            <option value="topk" selected>topk</option>
+            <option value="bottomk" selected>bottomk</option>
+        </select>
+    </div>
+
+    <div>{activation_text}</div>
+    <table class="tokens">
+        {#if focus_topk_tokens != null}
+            {#if filter == null || filter == "topk"}
+                {#each focus_topk_tokens as tok, tok_i}
+                    <tr
+                        ><td
+                            class="token"
+                            style="background: {toColor(
+                                topk_vals.get(tok_i, focus_layer, focus_obj)
+                            )}; text-align:center"
+                            on:mouseover={() => {
+                                onOver(
+                                    Number(
+                                        topk_vals.get(
+                                            tok_i,
+                                            focus_layer,
+                                            focus_obj
+                                        )
+                                    ).toFixed(2)
+                                );
+                            }}
+                            on:focus={() => {}}
+                            on:mouseleave={() => {
+                                onLeave(tok);
+                            }}>{tok}</td
+                        ></tr
+                    >
+                {/each}
+            {/if}
+            {#if filter == null}
+                <tr
+                    ><td
+                        class="ellipsis"
+                        style="background: #FFFFFF; text-align:center; font-weight: bold"
+                    >
+                        ...
+                    </td></tr
+                >
+            {/if}
+            {#if filter == null || filter == "bottomk"}
+                {#each focus_bottomk_tokens as tok, tok_i}
+                    <tr
+                        ><td
+                            class="token"
+                            style="background: {toColor(
+                                bottomk_vals.get(tok_i, focus_layer, focus_obj)
+                            )}; text-align:center"
+                            on:mouseover={() => {
+                                onOver(
+                                    Number(
+                                        bottomk_vals.get(
+                                            tok_i,
+                                            focus_layer,
+                                            focus_obj
+                                        )
+                                    ).toFixed(2)
+                                );
+                            }}
+                            on:focus={() => {}}
+                            on:mouseleave={() => {
+                                onLeave(tok);
+                            }}>{tok}</td
+                        ></tr
+                    >
+                {/each}
+            {/if}
+        {:else}
+            {#if filter == null || filter == "topk"}
+                {#each range(topk_inds.shape[0]) as _, tok_i}
+                    <tr style="text-align:center">
+                        {#each idx_into_arr_from_ndarray_slice(tokens, topk_inds.pick(tok_i, focus_layer, null)) as tok, dir_j}
+                            <td
+                                class="token"
+                                style="background: {toColor(
+                                    topk_vals.get(tok_i, focus_layer, dir_j)
+                                )}; text-align:center"
+                                on:mouseover={() => {
+                                    onOver(
+                                        Number(
+                                            topk_vals.get(
+                                                tok_i,
+                                                focus_layer,
+                                                dir_j
+                                            )
+                                        ).toFixed(2)
+                                    );
+                                }}
+                                on:focus={() => {}}
+                                on:mouseleave={() => {
+                                    onLeave(tok);
+                                }}>{tok}</td
+                            >
+                        {/each}
+                    </tr>
+                {/each}
+            {/if}
+            {#if filter == null}
+                <tr style="text-align:center">
+                    {#each range(topk_inds.shape[2]) as _, obj_i}
+                        <td
+                            class="ellipsis"
+                            style="background: #FFFFFF; text-align:center"
+                        >
+                            ----------
+                        </td>
+                    {/each}
+                </tr>
+            {/if}
+            {#if filter == null || filter == "bottomk"}
+                {#each range(bottomk_inds.shape[0]) as _, tok_i}
+                    <tr style="text-align:center">
+                        {#each idx_into_arr_from_ndarray_slice(tokens, bottomk_inds.pick(tok_i, focus_layer, null)) as tok, dir_j}
+                            <td
+                                class="token"
+                                style="background: {toColor(
+                                    bottomk_vals.get(tok_i, focus_layer, dir_j)
+                                )}; text-align:center"
+                                on:mouseover={() => {
+                                    onOver(
+                                        Number(
+                                            bottomk_vals.get(
+                                                tok_i,
+                                                focus_layer,
+                                                dir_j
+                                            )
+                                        ).toFixed(2)
+                                    );
+                                }}
+                                on:focus={() => {}}
+                                on:mouseleave={() => {
+                                    onLeave(tok);
+                                }}>{tok}</td
+                            >
+                        {/each}
+                    </tr>
+                {/each}
+            {/if}
+        {/if}
+    </table>
+</div>
+
+<style>
+    .title {
+        margin-top: 5px;
+        font-weight: bold;
+        font-size: 15px;
+    }
+    .token {
+        color: black;
+        overflow: hidden;
+    }
+    .ellipsis {
+        color: black;
+        overflow: hidden;
+    }
+    .tokens {
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        font-family: sans-serif;
+        min-width: 100px;
+        text-align: center;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    }
+
+    table tr td {
+        opacity: 0.9;
+    }
+
+    table tr td.token:hover {
+        outline: 1px solid #000;
+    }
+</style>
